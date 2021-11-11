569a79225d4ea768d761a834d16d5245346d192e
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-11-11 23:49:15 +0500

    added lower()

--- test.py
+++ test.py
@@ -1,11 +1,32 @@
-print("Welcome to my computer quiz!")
+print("Welcome To my computer quiz!")
+
 playing = input("Do you want to play?")
-if playing != 'yes':
+
+if playing.lower() != 'yes':
     quit()
 
 print("Okay! Let's play: )")
+
 answer = input('What does CPU stand for? ')
-if answer == "central processing unit":
+if answer.lower() == "central processing unit":
+    print('Correct!')
+else:
+    print('Incorrect!')
+
+answer = input('What does GPU stand for? ')
+if answer.lower() == "graphics processing unit":
+    print('Correct!')
+else:
+    print('Incorrect!')
+
+answer = input('What does RAM stand for? ')
+if answer.lower() == "random access memory":
+    print('Correct!')
+else:
+    print('Incorrect!')
+
+answer = input('What does PSU stand for? ')
+if answer.lower() == "power supply":
     print('Correct!')
 else:
     print('Incorrect!')
