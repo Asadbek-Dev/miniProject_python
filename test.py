199843da89b5970499ad983f74fac7a9320fd883
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-11-11 22:31:43 +0500

    first commit

---
+++ test.py
@@ -0,0 +1,11 @@
+print("Welcome to my computer quiz!")
+playing = input("Do you want to play?")
+if playing != 'yes':
+    quit()
+
+print("Okay! Let's play: )")
+answer = input('What does CPU stand for? ')
+if answer == "central processing unit":
+    print('Correct!')
+else:
+    print('Incorrect!')
