6b3596f68db5a941f95af50ec2ea4fb988d213d4
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-11-19 22:24:36 +0500

    changed

---
+++ choosing_game.py
@@ -0,0 +1,28 @@
+name = input("Type your name: ")
+print("Welcome", name, "to this adventure!")
+
+answer = input(
+    "You are on a dirt road, it has come to an end you can go left or right.Which way would you like to go? ").lower()
+
+if answer == "left":
+    answer = input(
+        "You come to a river, you can walk around it or swim accross? Type walk to walk around and swim to swim accross: ")
+
+    if answer == "swim":
+        print("You swam across and were eaton by an alligator.")
+    elif answer == "walk":
+        print("You walked for many miles, ran out of water and you lost the game.")
+    else:
+        print("Not a valid option. You lose.")
+elif answer == 'right':
+    answer = input(
+        "You come to a bridge, it looks wobbly, do you want to cross it or head back (cross/back)?")
+
+    if answer == "back":
+        print("You swam across and were eaton by an alligator.")
+    elif answer == "cross":
+        print("You walked for many miles, ran out of water and you lost the game.")
+    else:
+        print("Not a valid option. You lose.")
+else:
+    print('Not a valid option. You lose.')
