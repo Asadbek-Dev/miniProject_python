66f76c6604668088a25e92c392ee6a02ed250ff5
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-11-19 21:14:22 +0500

    completed

--- rock_paper.py
+++ rock_paper.py
@@ -20,17 +20,16 @@ while True:
     if user_input == 'rock' and computer_pick == 'scissors':
         print("You won!")
         user_wins += 1
-        continue
 
-    if user_input == 'paper' and computer_pick == 'rock':
+    elif user_input == 'scissors' and computer_pick == 'paper':
         print("You won!")
         user_wins += 1
-        continue
 
-    if user_input == 'scissors' and computer_pick == 'paper':
-        print("You won!")
-        user_wins += 1
-        continue
+    else:
+        print("You lost!")
+        computer_wins += 1
 
 
-print("Goodbye !")
+print("You won", user_wins, 'times.')
+print("The computer won", computer_wins, "times.")
+print("Goodbye!")
