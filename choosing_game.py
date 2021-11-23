2069c34ca091fdc40fd0e7d06a0d16f0bdc9bc11
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-11-23 23:16:41 +0500

    completed 4th game

--- choosing_game.py
+++ choosing_game.py
@@ -21,8 +21,18 @@ elif answer == 'right':
     if answer == "back":
         print("You go back and lose.")
     elif answer == "cross":
-        print("You walked for many miles, ran out of water and you lost the game.")
+        answer = input(
+            "You cross the bridge and meet a stranger. Do you talk to them(yes/no)?")
+        if answer == 'yes':
+            print("You talk to the stranger and they give you gold. You WIN!")
+        elif answer == 'no':
+            print("You ignore the stranger and they are offended and you lose.")
+        else:
+            print("Not a valid option. You lose.")
     else:
         print("Not a valid option. You lose.")
 else:
     print('Not a valid option.You lose.')
+
+
+print("Thank you for trying", name)
